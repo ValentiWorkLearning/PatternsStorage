afee26b4d945106f9832b4af6dc447b86359581c
Valentyn Korniienko <kornienko-vr@rambler.ru>
2019-07-01 23:37:12 +0300

    Add project files.

---
+++ TestCmake/TestCmake.cpp
@@ -0,0 +1,12 @@
+﻿// TestCmake.cpp : Defines the entry point for the application.
+//
+
+#include "TestCmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
